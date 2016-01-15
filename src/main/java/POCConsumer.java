68a9cf9c9db80e2b72c63eaa44538b4e82b4debf
facundoj <jaureguifacundo@gmail.com>
2016-01-14 23:00:16 -0300

    Progress

--- src/main/java/POCConsumer.java
+++ src/main/java/POCConsumer.java
@@ -1,23 +1,30 @@
-import com.clearspring.analytics.util.Lists;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFlatMapFunction;
+import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.streaming.Duration;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaPairReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.kafka.KafkaUtils;
 import org.codehaus.jackson.JsonProcessingException;
 import org.imsglobal.caliper.entities.LearningObjective;
 import org.imsglobal.caliper.entities.assessment.AssessmentItem;
+import org.imsglobal.caliper.entities.assignable.Attempt;
+import org.imsglobal.caliper.entities.outcome.Result;
+import org.imsglobal.caliper.entities.response.Response;
 import org.imsglobal.caliper.events.AssessmentItemEvent;
 import org.imsglobal.caliper.events.BaseEventContext;
 import org.imsglobal.caliper.events.EventType;
+import org.imsglobal.caliper.events.OutcomeEvent;
 import org.k12.caliper.poc.parser.CaliperParser;
 import org.k12.caliper.poc.parser.CaliperParserFactory;
 import scala.Tuple2;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -51,27 +58,103 @@ public class POCConsumer {
 
         // START: Running total - Spark logic **********************************
 
-        caliperEvents.flatMap(new FlatMapFunction<Tuple2<String,String>, Tuple2<String,String>>() {
-            public Iterable<Tuple2<String,String>> call(Tuple2<String, String> msg) throws Exception {
+        // items -- (key, (usr, [LOs]))
+        JavaPairDStream<String, Tuple2<String, Iterable<String>>> itemsStream = caliperEvents.mapToPair(new PairFunction<Tuple2<String,String>, String, Tuple2<String, Iterable<String>>>() {
+            public Tuple2<String, Tuple2<String, Iterable<String>>> call(Tuple2<String, String> msg) throws Exception {
                 BaseEventContext event = getCaliperModel(msg._2());
 
+                // Filter AssessmentItemEvents
                 if (EventType.ASSESSMENT_ITEM.getValue().equals(event.getType())) {
                     AssessmentItemEvent aiEvent = (AssessmentItemEvent) event;
-                    AssessmentItem object = (AssessmentItem) aiEvent.getObject();
-                    List<Tuple2<String,String>> output = Lists.newArrayList();
+                    AssessmentItem ai = (AssessmentItem) aiEvent.getObject();
+                    Response response = (Response) aiEvent.getGenerated();
 
-                    for (LearningObjective lo : object.getLearningObjectives()) {
-                        output.add(new Tuple2<String, String>(object.getId(), lo.getId()));
+                    // key: {attemptId}-{itemId}
+                    String key = response.getAttempt().getId() + "-" + ai.getId();
+
+                    List<String> learningObjectivesIds = new ArrayList<String>();
+                    for (LearningObjective lo : ai.getLearningObjectives()) {
+                        learningObjectivesIds.add(lo.getId());
                     }
 
-                    return output;
+                    return new Tuple2<String, Tuple2<String, Iterable<String>>>(
+                            key,
+                            new Tuple2<String, Iterable<String>>(
+                                    aiEvent.getActor().getId(), // Student
+                                    learningObjectivesIds // LOs
+                            )
+                    );
                 }
 
                 return null;
             }
         });
 
-        caliperEvents.print();
+        // results -- (key, (usr, (obtained, total)))
+        JavaPairDStream<String, Tuple2<String, Tuple2<Double, Double>>> resultsStream = caliperEvents.mapToPair(new PairFunction<Tuple2<String, String>, String, Tuple2<String, Tuple2<Double, Double>>>() {
+            public Tuple2<String, Tuple2<String, Tuple2<Double, Double>>> call(Tuple2<String, String> msg) throws Exception {
+                BaseEventContext event = getCaliperModel(msg._2());
+
+                // Filter OutcomeEvents
+                if (EventType.OUTCOME.getValue().equals(event.getType())) {
+                    OutcomeEvent oEvent = (OutcomeEvent) event;
+                    Attempt attempt = (Attempt) oEvent.getObject();
+                    AssessmentItem ai = (AssessmentItem) oEvent.getTarget();
+                    Result result = (Result) oEvent.getGenerated();
+
+                    // key: {attemptId}-{itemId}
+                    String key = attempt.getId() + "-" + ai.getId();
+
+                    return new Tuple2<String, Tuple2<String, Tuple2<Double, Double>>>(
+                            key,
+                            new Tuple2<String, Tuple2<Double, Double>>(
+                                    oEvent.getActor().getId(), // Student
+                                    new Tuple2<Double, Double>( // Performance
+                                            result.getNormalScore(), // Obtained
+                                            result.getTotalScore() // Total
+                                    )
+                            )
+                    );
+                }
+                return null;
+            }
+        });
+
+        // joined -- (key, ( (usr, [LOs]), (usr, (obtained, total)) ))
+        JavaPairDStream<String, Tuple2<Tuple2<String, Iterable<String>>, Tuple2<String, Tuple2<Double, Double>>>> joined = itemsStream.join(resultsStream);
+
+        // result -- ((usr, loId), (obtained, total))
+        JavaPairDStream<Tuple2<String, String>, Tuple2<Double, Double>> resultFlatten = joined.flatMapToPair(new PairFlatMapFunction<Tuple2<String, Tuple2<Tuple2<String, Iterable<String>>, Tuple2<String, Tuple2<Double, Double>>>>, Tuple2<String, String>, Tuple2<Double, Double>>() {
+            public Iterable<Tuple2<Tuple2<String, String>, Tuple2<Double, Double>>> call(Tuple2<String, Tuple2<Tuple2<String, Iterable<String>>, Tuple2<String, Tuple2<Double, Double>>>> tuple) throws Exception {
+                List<Tuple2<Tuple2<String, String>, Tuple2<Double, Double>>> out = new ArrayList<Tuple2<Tuple2<String, String>, Tuple2<Double, Double>>>();
+
+                String user = tuple._2()._1()._1();
+                Tuple2<Double, Double> performance = tuple._2()._2()._2();
+
+                for (String loId : tuple._2()._1()._2()) {
+                    out.add(
+                            new Tuple2<Tuple2<String, String>, Tuple2<Double, Double>>(
+                                    new Tuple2<String, String>(user, loId), // (Student, LO)
+                                    performance // (Obtained, Total)
+                            )
+                    );
+                }
+
+                return out;
+            }
+        });
+
+        // result -- ((usr, loId), (obtained, total))
+        JavaPairDStream<Tuple2<String, String>, Tuple2<Double, Double>> result = resultFlatten.reduceByKey(new Function2<Tuple2<Double, Double>, Tuple2<Double, Double>, Tuple2<Double, Double>>() {
+            public Tuple2<Double, Double> call(Tuple2<Double, Double> value1, Tuple2<Double, Double> value2) throws Exception {
+                return new Tuple2<Double, Double>(
+                        value1._1() + value2._1(), // Total obtained
+                        value1._2() + value2._2() // Accumulated total
+                );
+            }
+        });
+
+        result.print();
 
         // END: Running total - Spark logic ************************************
 
